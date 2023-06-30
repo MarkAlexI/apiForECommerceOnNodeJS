7b8080576cb874e701812b1b9bd06aae6822befe
MarkAlexI <markovaleksandr108@gmail.com>
2023-07-01 02:24:40 +0300

    Added method for creating user. Generating hashed password for every user.

--- src/users/user.datastorage.ts
+++ src/users/user.datastorage.ts
@@ -1,5 +1,7 @@
 import { User, UnitUser, Users } from "./user.interface";
 import fs from "fs";
+import bcrypt from "bcryptjs";
+import { v4 as random } from "uuid";
 
 let users: Users = loadUsers(); 
 
@@ -25,3 +27,32 @@ function saveUsers() {
 export const findAll = async (): Promise<UnitUser[]> => Object.values(users);
 
 export const findOne = async (id: string): Promise<UnitUser> => users[id];
+
+export const create = async (userData: UnitUser): Promise<>
+
+  let id = random();
+
+  let check_user = await findOne(id);
+
+  while (check_user) {
+    id = random();
+    check_user = await findOne(id);
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  const hashedPassword = await bcrypt.hash(userData.passwo>
+
+  const user: UnitUser = {
+    id: id,
+    username: userData.username,
+    email: userData.email,
+    password: hashedPassword
+  };
+
+  users[id] = user;
+
+  saveUsers();
+
+  return user;
+};
